621a1901ebb1911d2e40907f39ca95822432d153
Christian <ChQu00223@gmail.com>
2020-11-23 14:48:23 -0400

    init commit

---
+++ streams/client/src/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => <div>PageOne</div>;
+const App = () => <div>App</div>;
+
+export default App;
